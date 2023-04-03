be9b21226c3a7a885dc1ee308d52ea4cf90f1ff5
anseriodflowers <anseriodflowers@gmail.com>
2023-04-03 17:03:38 -0400

    commit

---
+++ variable.tf
@@ -0,0 +1,9 @@
+variable "access_key" {
+  default = "AKIAVW373IOJVZFJSHHR"
+  }
+variable "secret_key" {
+  default = "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"
+  }
+variable "region" {
+  default = "us-east-1"
+  }
