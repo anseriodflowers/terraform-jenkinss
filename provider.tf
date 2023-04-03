68fed0437370c8cc9ee083cafc168ceb18d10879
anseriodflowers <anseriodflowers@gmail.com>
2023-04-03 19:48:56 -0400

    add ec2

--- provider.tf
+++ provider.tf
@@ -5,3 +5,14 @@ provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_key
   }
+
+
+# Resource: EC2 Instance
+resource "aws_instance" "myec2vm" {
+  ami = "ami-04581fbf744a7d11f"
+  instance_type = "t2.micro"
+ # user_data = file("app1-install.sh")
+  tags = {
+    "Name" = "EC2 Demo"
+  }
+}
