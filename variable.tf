aed4cc398d67c4080d24210ff442de1042d629ce
anseriodflowers <128719869+anseriodflowers@users.noreply.github.com>
2023-04-04 15:33:17 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,11 +1,11 @@
 ##Variables only used by myself
 
-variable "access_key" {
-  default = "AKIAVW373IOJ77ZJUZE2"
-  }
-variable "secret_key" {
-  default = "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"
-  }
+# variable "access_key" {
+#   default = "AKIAVW373IOJ77ZJUZE2"
+#   }
+# variable "secret_key" {
+#   default = "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"
+#   }
 variable "region" {
   default = "us-east-1"
   }
