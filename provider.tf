d08dbdc7d3ea72a9bbec3c8147866d59733c6e1f
anseriodflowers <128719869+anseriodflowers@users.noreply.github.com>
2023-04-18 17:00:13 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,19 +1,17 @@
-# # Terraform Block
-# terraform {
-#   required_version = ">= 1.0" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
-#   required_providers {
-#     aws = {
-#       source = "hashicorp/aws"
-#       version = "~> 3.0"
-#     }
-#   } 
-# } 
+# Terraform Block
+terraform {
+  required_version = ">= 1.0" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  } 
+} 
 
 ##GitHub Variable Keys
 provider "aws" {
   region     = var.region
-#   access_key = var.access_key
-#   secret_key = var.secret_key
   }
 
 
@@ -22,7 +20,6 @@ resource "aws_instance" "myec2vm" {
   ami = "ami-04581fbf744a7d11f"
   instance_type = "t2.micro"
   user_data = file("app1-install.sh")
- # user_data = file("app1-install.sh")
   tags = {
     "Name" = "Jenkins - Terraform Demo"
   }
